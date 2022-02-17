c0c3c7eda95b7977456e38702302c1a017686f11
Rafael Alcântara <devrafasouza@gmail.com>
2022-02-17 10:23:22 -0300

    Adicionada as funcionalidades: Criar um usuario, Criar um todo, Consultar as todos e um middleware para verificar se o usuario já existe

--- src/index.js
+++ src/index.js
@@ -11,35 +11,72 @@ app.use(express.json());
  const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(400).json({error: "Usuario não existe"});
+  }
+  
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
-  users.push({
+  const usernameExiste = users.some((user) => user.username === username);
+  
+
+  if(usernameExiste){
+    return response.status(400).json({error: "Esse username já está em uso!"});
+  }
+
+  const user = {
     id: uuidv4,
     name,
     username,
     todos: []
-  })
+  }
 
-  return response.status(201).send();
+  users.push(user);
+
+  return response.status(201).json(user);
   
 });
 
 
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.status(201).json(user.todos)
+  
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4,
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
